2dcb580ee884263bf44c938a42cd6a0dfdeeb0dd
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-05-21 22:46:05 -0300

    feat: check if savefile exists

--- scripts/menu.py
+++ scripts/menu.py
@@ -1,15 +1,20 @@
-
 import bge
+from bge.types import *
+from pathlib import Path
+from bge.logic import expandPath
 
 def start(cont):
+    # type: (SCA_PythonController) -> None
     pass
 
 def update(cont):
+    # type: (SCA_PythonController) -> None
+
     own = cont.owner
     print('ok')
-    m_over = cont.sensors['over']
-    m_left = cont.sensors['mouse']
-    menu_pos = own.childrenRecursive.get('menu_pos')
+    m_over = cont.sensors['over'] # type: KX_MouseFocusSensor
+    m_left = cont.sensors['mouse'] # type: SCA_MouseSensor
+    menu_pos = own.childrenRecursive.get('menu_pos') # type: KX_GameObject
 
     if m_over.positive:
         if m_left.positive:
@@ -18,7 +23,9 @@ def update(cont):
                 menu_pos.worldPosition = [-4.0,0.0,0.0]
 
             if 'load' in m_over.hitObject:
-                bge.logic.globalDict['load'] = -5
-                bge.logic.addScene('Game',0)
+                saveFile = Path(expandPath('//save.txt'))
 
+                if saveFile.exists():
+                    bge.logic.globalDict['load'] = -5
+                    bge.logic.addScene('Game', 0)
 
